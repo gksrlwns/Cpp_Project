6c487b02db214ece85e55a956f6055d55838bf6c
gksrlwns <83814481+gksrlwns@users.noreply.github.com>
2024-10-02 21:41:10 +0900

    17

--- Academy/WindowsGame/WindowsMain.cpp
+++ Academy/WindowsGame/WindowsMain.cpp
@@ -1,5 +1,5 @@
 ﻿#include "pch.h"
-
+#include "Game.h"
 
 //전역변수:
 HINSTANCE	_hInstance;
@@ -25,11 +25,26 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	}
 
 	// 3. 윈도우 메세지 루프
-	MSG msg;
-	while (GetMessage(&msg, nullptr, 0, 0))
+	MSG msg = {};
+	Game game;
+
+	game.Init(_hWnd);
+
+	//GetMessage -> 메세지가 올떄까지 기다려요. (scanf마냥 기다립니다 메세지올때까지)
+	//PeekMessage -> 메세지가 안오면 그냥 null처리하고 넘어갑니다.
+
+	while (msg.message != WM_QUIT)
 	{
-		::TranslateMessage(&msg);
-		::DispatchMessage(&msg);
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			::TranslateMessage(&msg);
+			::DispatchMessage(&msg);
+		}
+		else
+		{
+			game.Update();
+			game.Render();
+		}
 	}
 }
 
@@ -88,63 +103,14 @@ void SetWindowSize(int x, int y, int width, int height)
 	::SetWindowPos(_hWnd, nullptr, x, y, rc.right - rc.left, rc.bottom - rc.top, SWP_NOZORDER);
 }
 
-POINT mousePos = {};
-
-class Enemy
-{
-public:
-	RECT rc;
-	bool isLive;
-
-	void Die()
-	{
-		isLive = false;
-	}
-	void Draw(HDC hdc)
-	{
-		::Rectangle(hdc, rc.left, rc.top, rc.right, rc.bottom);
-	}
-	bool IsLive()
-	{
-		return isLive;
-	}
-};
-
-vector<Enemy> enemies;
-
 LRESULT WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
 	{
 	case WM_CREATE:
-		// 윈도우가 생성되었을때 발생하는 이벤트
-		// 여기에서 초기화 동작
 		srand(time(NULL));
-
-		//60프레임 -> 타이머호출주기
-		SetTimer(hWnd, 1, 1000 / 60, NULL);
-
-		//두더지 튀어나오기
-		SetTimer(hWnd, 2, 1000, NULL);
-
-		for (int i = 0; i < 9; i++)
-		{
-			Enemy newEnemy;
-			newEnemy.isLive = false;
-			newEnemy.rc = {
-				(i % 3) * 100 ,
-				(i / 3) * 100,
-				(i % 3) * 100 + 70,
-				(i / 3) * 100 + 70
-			};
-			enemies.push_back(newEnemy);
-		}
-
 		break;
 		
-	case WM_LBUTTONDOWN:
-
-		break;
 	case WM_PAINT:
 		{
 			// 화면을 그리는 이벤트
@@ -153,89 +119,12 @@ LRESULT WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			PAINTSTRUCT ps;
 			HDC hdc = ::BeginPaint(hWnd, &ps);
 
-			for (Enemy enemy : enemies)
-			{
-				if (enemy.IsLive())
-				{
-					enemy.Draw(hdc);
-				}
-			}
-
 			::EndPaint(hWnd, &ps);
-		}
-		break;
-
-	case WM_KEYDOWN:
-	{
-		switch (wParam)
-		{
-		case 'A':
-			for (Enemy& enemy : enemies)
-			{
-				if (enemy.rc.left <= mousePos.x && mousePos.x <= enemy.rc.right)
-				{
-					if (enemy.rc.top <= mousePos.y && mousePos.y <= enemy.rc.bottom)
-					{
-						enemy.Die();
-					}
-				}
-			}
-			break;
-
-		case 'S':
-			for (Enemy& enemy : enemies)
-			{
-				printf("-----------------------\n");
-				printf("rc : %d, %d, %d, %d\n", enemy.rc.left, enemy.rc.top, enemy.rc.right, enemy.rc.bottom);
-				printf("isLive : %d\n", enemy.isLive);
-				printf("-----------------------\n");
-			}
-			break;
-		default:
-			break;
-		}
-	}
-		break;
-
-	case WM_MOUSEMOVE:
-	{
-		mousePos.x = GET_X_LPARAM(lParam);
-		mousePos.y = GET_Y_LPARAM(lParam);
-	}
-		break;
-
-	case WM_TIMER:
-		switch (wParam)
-		{
-		case 1:
-			InvalidateRect(hWnd, NULL, true);
-			break;
-
-		case 2:
-			// 두더지 튀어나오는 로직
-			// SetTimer(hWnd, 두더지번호 * 100, 5000, NULL);
-			// 100 , 200 , 300, 400, 500, 600, 700, 800, 900 으로 만들어지겠죠
-			//  + 두더지 잡았을때 KillTimer를 꼭해주셔야합니다. (두더지번호 * 100)
-			break;
-
-		case 100:
-			//두더지 들어가는 로직
-			break;
-		case 200:
-			//두더지 들어가는 로직
-			break;
-		case 300:
-			//두더지 들어가는 로직
-			break;
-
-		default:
-			break;
 		}
 		break;
 
 	case WM_DESTROY:
 		// 윈도우 종료 메세지가 왔을때 발생되는 이벤트
-		::KillTimer(hWnd, 1);
 		::PostQuitMessage(0);
 		break;
 
