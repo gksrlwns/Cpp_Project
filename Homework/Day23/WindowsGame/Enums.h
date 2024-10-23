55ecf6bb24df4b569b724ad830125661918ee1c7
gksrlwns <83814481+gksrlwns@users.noreply.github.com>
2024-10-23 19:08:08 +0900

    Update Day23

---
+++ Homework/Day23/WindowsGame/Enums.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+enum class SceneType
+{
+	None,
+	DevScene,
+	Day17Scene,
+	Day18Scene,
+	Day19Scene,
+	Day20Scene,
+	Day21Scene,
+	Day22Scene,
+	Day23Scene,
+};
+
+enum class ColliderType
+{
+	None,
+	Box,
+	Circle
+};
+
+enum CollisionLayerType
+{
+	CLT_DEFAULT = 0,
+	CLT_CREATURE,
+	CLT_GROUND,
+	CLT_ITEM
+};
