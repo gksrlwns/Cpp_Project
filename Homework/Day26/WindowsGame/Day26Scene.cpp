298de866917b6be51925ebec069af00e77a7c97d
kijunHan <gksrlwns96@naver.com>
2024-11-01 21:46:51 +0900

    Update: Day26

---
+++ Homework/Day26/WindowsGame/Day26Scene.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "Day26Scene.h"
+#include "Tilemap.h"
+void Day26Scene::Init()
+{
+	Super::Init();
+
+
+	//Tilemap 로드
+	Tilemap* tilemap = Resource->LoadTilemap(L"TM_Test", L"Day26/map.txt");
+
+	cout << tilemap->GetMapSize().x << "\t" << tilemap->GetMapSize().y << endl;
+}
+void Day26Scene::Render(HDC hdc)
+{
+	Super::Render(hdc);
+	//씬 이름 출력
+	{
+		wstring nameStr = ::format(L"Day26Scene");
+		Draw::Text(hdc, 0, 20, nameStr);
+	}
+}
+void Day26Scene::Update()
+{
+	Super::Update();
+}
+void Day26Scene::Release()
+{
+	Super::Release();
+}
---
+++ Homework/Day26/WindowsGame/Scene.cpp
@@ -0,0 +1,92 @@
+﻿#include "pch.h"
+#include "Scene.h"
+#include "GameObject.h"
+void Scene::Init()
+{
+
+}
+void Scene::Render(HDC hdc)
+{
+	{
+		int size = _gameObjects.size();
+		for (int i = 0; i < size; i++)
+		{
+			GameObject* gameObject = _gameObjects[i];
+			if (gameObject == nullptr) continue;
+			gameObject->Render(hdc);
+		}
+	}
+
+	//for (GameObject* gameObject : _gameObjects)
+	//{
+	//	if (gameObject == nullptr) continue;
+	//	gameObject->Render(hdc);
+	//}
+}
+void Scene::Update()
+{
+	{
+		int size = _gameObjects.size();
+		for (int i = 0; i < size; i++)
+		{
+			GameObject* gameObject = _gameObjects[i];
+			if (gameObject == nullptr) continue;
+			gameObject->Update();
+		}
+	}
+	//for (GameObject* gameObject : _gameObjects)
+	//{
+	//	if (gameObject == nullptr) continue;
+	//	gameObject->Update();
+	//}
+
+	while (false == _despawnObjectList.empty())
+	{
+		GameObject* deleteGameObject = _despawnObjectList.front();
+		_despawnObjectList.pop();
+
+		auto findIt = find(_gameObjects.begin(), _gameObjects.end(), deleteGameObject);
+
+		if (findIt != _gameObjects.end())
+		{
+			deleteGameObject->Release();
+			SAFE_DELETE(deleteGameObject);
+			_gameObjects.erase(findIt);
+		}
+	}
+}
+void Scene::Release()
+{
+	for (GameObject* gameObject : _gameObjects)
+	{
+		gameObject->Release();
+		SAFE_DELETE(gameObject);
+	}
+	_gameObjects.clear();
+}
+
+void Scene::SpawnGameObject(GameObject* gameObject)
+{
+	if (gameObject == nullptr) return;
+
+	_gameObjects.push_back(gameObject);
+}
+void Scene::DespawnGameObject(GameObject* gameObject)
+{
+	if (gameObject == nullptr) return;
+
+	_despawnObjectList.push(gameObject);
+}
+
+GameObject* Scene::FindGameObject(string name)
+{
+	for (GameObject* gameObject : _gameObjects)
+	{
+		if (gameObject->GetName() == name)
+		{
+			return gameObject;
+		}
+	}
+
+	return nullptr;
+}
